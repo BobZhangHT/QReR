3ba352c5dafcee57742ab04b5072a616752dd2e7
bobzht <bobzht341@gmail.com>
2020-12-09 15:15:20 +0800

    add new matching methods and DR

--- benchmarks.py
+++ benchmarks.py
@@ -37,11 +37,16 @@ def glm_wrapper(x,z):
     
     return glm_ps, ps_score
     
-def match_wrapper(y,z,X):
+def match_wrapper(y,z,X,Z=None,bias_adj=False):
+    if Z is None:
+        Z = X
+    # add bias-adjustment
     kwargs = {'Y':y,
           'Tr':z,
+          'Z':Z,
           'X':X,
           'estimand':'ATE',
+          'BiasAdjust':bias_adj,
           'M':1}
     rr = matching.Match(**kwargs)
     tauhat = rr[0]
@@ -69,3 +74,33 @@ def ipw1_wrapper(y,z,x):
     ub = tauhat + qnorm*se
     
     return tauhat, lb, ub
+
+def dr_wrapper(y,z,x):
+    from sklearn.linear_model import LinearRegression
+    
+    # estimate propensity score
+    glm_ps, ps_score = glm_wrapper(x,z)
+
+    # estimate linear model for each treatment group
+    lr1 = LinearRegression()
+    lr0 = LinearRegression()
+    lr1.fit(x[z==1,:],y[z==1])
+    lr0.fit(x[z==0,:],y[z==0])
+
+    yhat1 = lr1.predict(x)
+    yhat0 = lr0.predict(x)
+
+    # point estimator
+    tauhat = np.mean((z*y-(z-ps_score)*yhat1)/ps_score)-np.mean(((1-z)*y+(z-ps_score)*yhat0)/(1-ps_score))
+
+    # compute the se (following Gutman & Rubin SMMR 2015)
+    I = (z*y-(z-ps_score)*yhat1)/ps_score-((1-z)*y+(z-ps_score)*yhat0)/(1-ps_score)-tauhat
+    se = np.sum(I**2)/(I.shape[0]**2)
+
+    # confidence interval
+    qnorm = stats.qnorm(0.975).item()
+    lb = tauhat - qnorm*se
+    ub = tauhat + qnorm*se
+    
+    return tauhat, lb, ub
+    
